5d879ef9b88841ac7420849a6c06661177b43ba7
ABHAY CHAUDHARY <abhaychaudhary343434@gmail.com>
2026-01-07 08:50:24 +0000

    ADD TEST PROGRAM

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("ENTER TWO NUMBERS:");
+int x=sc.nextInt();
+int y=sc.nextInt();
+System.out.print("SUM="+(x+y));
+}
+}
